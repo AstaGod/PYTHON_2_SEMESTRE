65403ae00b0a2d0d3db2067a4925dd4929a492ec
AstaGod <bautistapomacrhistianalexis@gmail.com>
2023-10-18 12:28:49 -0500

    clase POO_LP 18 de octubre mini actualizacion

--- POO_LP/lambda.py
+++ POO_LP/lambda.py
@@ -14,3 +14,40 @@ if True:
     print("soy verdad")
 else:
     print("soy mentira")
+
+lista_alumnos=[
+    {
+        "nombre":"edwin",
+        "edad":15,
+        "hobby":"danza",
+        "flaquita":"melody"
+    },
+    {
+        "nombre":"del mar",
+        "edad":30,
+        "hobby":"rodar",
+        "flaquita":"melody"
+    },
+    {
+        "nombre":"orlando",
+        "edad":14,
+        "hobby":"ponchar",
+        "flaquita":"sami"
+    },
+    {
+        "nombre":"jory",
+        "edad":50,
+        "hobby":"danza",
+        "flaquita":"xd"
+    },
+    {
+        "nombre":"hans",
+        "edad":12,
+        "hobby":"quemarse",
+        "flaquita":"hermana"
+    }
+]
+
+print(f"todos mis alumnitos{lista_alumnos}")
+fans_melody=list(filter(lambda par:par["flaquita"]=="melody",lista_alumnos))
+print(f"los que quieren con melody {fans_melody}")
