7058221044f3fdd1c7e6fb9e9b348cade19ee1df
AstaGod <bautistapomacrhistianalexis@gmail.com>
2023-10-18 12:56:50 -0500

    clase POO_LP 18 de octubre mini actualizacion

--- POO_LP/lambda.py
+++ POO_LP/lambda.py
@@ -51,3 +51,31 @@ lista_alumnos=[
 print(f"todos mis alumnitos{lista_alumnos}")
 fans_melody=list(filter(lambda par:par["flaquita"]=="melody",lista_alumnos))
 print(f"los que quieren con melody {fans_melody}")
+
+nuevo_objet=list(map(lambda par:{"nombre_alumno":par["nombre"],"germita" :par["flaquita"]},lista_alumnos))
+print(nuevo_objet)
+
+## Tarea
+#### 1. crear una lista con 10 objetos que contengan los datos de la tiendas comerciales
+#-> ejemplo:
+
+tiendas=[
+    {
+        "ruc":465463213456,
+        "nombre":"el pichilon",
+        "categoria":["abarrotes"],
+        "horario_atencion":{
+            "dia":"7am-12pm",
+            "tarde":"2pm-8pm"
+        }
+        "gerente":"nadine"
+    }
+]
+
+# observacion : las categorias sera 4: abarrotes, farmacia, bodega, restaurant
+# observacion : los gerentes solo podran ser los siguientes: edwin,china,crhistian,nadine
+# realizar los siguientes ejercicios
+## crear una clase para tiendas qwue tenga los siguientes metodos o casos de uso.
+# 1. crear un metodo que me filtre las tiendas que tiene cada gerente
+# 2. crear un metodo que me muestre lso negocios que tienen mas de dos categorias
+# 3. crear un metodo que me muestre solo el nombre y ruc de la tienda
