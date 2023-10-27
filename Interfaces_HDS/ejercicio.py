80f55ad736e8df123022ecc8fc619112f222905d
AstaGod <bautistapomacrhistianalexis@gmail.com>
2023-10-27 09:59:37 -0500

    Clase de Radiobutthon dia 27 de octubre

--- Interfaces_HDS/ejercicio.py
+++ Interfaces_HDS/ejercicio.py
@@ -168,8 +168,7 @@
 #         mensaje.pack()   
 #     else:
 #         mensaje=Label(ventana,text="eres wawa")      
-#         mensaje.pack()  
-                                   
+#         mensaje.pack()                                   
 # etiqueta=Label(ventana,text="introduzca tu edad ")
 # etiqueta.pack()
 # text_edad=Entry(ventana)
@@ -183,38 +182,40 @@
 
 # EJERCICIO 6
 
-from tkinter import*
-ventana=Tk()
-ventana.geometry("250x300")
-ventana.title("Papu misterioso")
+# from tkinter import*
+# ventana=Tk()
+# ventana.geometry("250x300")
+# ventana.title("Papu misterioso")
 
-def comprobar_datos():
-    text1=text_usuario.get()
-    text2=int(text_contra.get())
-    if text1 == usuario and text2==contra:
-        mensaje=Label(ventana,text="Vienvenido csmr :)")      
-        mensaje.pack()
-    else:
-        mensaje=Label(ventana,text="quien eres csmr")      
-        mensaje.pack()
+# def comprobar_datos():
+#     text1=text_usuario.get()
+#     text2=int(text_contra.get())
+#     if text1 == usuario and text2==contra:
+#         mensaje=Label(ventana,text="Vienvenido csmr :)")      
+#         mensaje.pack()
+#     else:
+#         mensaje=Label(ventana,text="quien eres csmr")      
+#         mensaje.pack()
 
-usuario="Asta"
-contra=71439102
+# usuario="Asta"
+# contra=71439102
 
-etiqueta1=Label(ventana,text="Introdusca su nombre de usuario")
-etiqueta1.pack()
+# etiqueta1=Label(ventana,text="Introdusca su nombre de usuario")
+# etiqueta1.pack()
 
-text_usuario=Entry(ventana)
-text_usuario.config(bg="red",fg="white")
-text_usuario.pack()
+# text_usuario=Entry(ventana)
+# text_usuario.config(bg="red",fg="white")
+# text_usuario.pack()
 
-etiqueta2=Label(ventana,text="Ingrese su contra")
-etiqueta2.pack()
+# etiqueta2=Label(ventana,text="Ingrese su contra")
+# etiqueta2.pack()
 
-text_contra=Entry(ventana)
-text_contra.config(bg="red",fg="white",show="*")
-text_contra.pack()
-boton_capturar=Button(ventana,text="Comprovar",command=comprobar_datos)
-boton_capturar.pack()
+# text_contra=Entry(ventana)
+# text_contra.config(bg="red",fg="white",show="*")
+# text_contra.pack()
+# boton_capturar=Button(ventana,text="Comprovar",command=comprobar_datos)
+# boton_capturar.pack()
 
-ventana.mainloop()
+# ventana.mainloop()
+
+# EJERCICIO 7
