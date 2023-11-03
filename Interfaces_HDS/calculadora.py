73618a22df2137f57d2eb0b340c4bffff2923d53
AstaGod <bautistapomacrhistianalexis@gmail.com>
2023-11-03 08:03:41 -0500

    Calculadora

--- Interfaces_HDS/calculadora.py
+++ Interfaces_HDS/calculadora.py
@@ -1,96 +1,52 @@
-from tkinter import Tk,Text,Button,END,re
+from tkinter import *
 
-class Interfaz:
-    def __init__(self, ventana):
-        #Inicializar la ventana con un título
-        self.ventana=ventana
-        self.ventana.title("Calculadora")
+ventana = Tk()
+ventana.title("Calculadora")
 
-        #Agregar una caja de texto para que sea la pantalla de la calculadora
-        self.pantalla=Text(ventana, state="disabled", width=40, height=3, background="orchid", foreground="white", font=("Helvetica",15))
+# class Funciones y Operaciones:
+widget1 = Entry(ventana)
+widget1.grid(row=0, column=0, columnspan=4, sticky=W + E)
+i=0
 
-        #Ubicar la pantalla en la ventana
-        self.pantalla.grid(row=0, column=0, columnspan=4, padx=5, pady=5)
+def iportar_numero(n):
+    global i
+    widget1.insert(i,n)
+    i+=1
 
-        #Inicializar la operación mostrada en pantalla como string vacío
-        self.operacion=""
+def operaciones(operador):
+    global i
+    longitud_operador=len(operador)
+    widget1.insert(i,operador)
+    i+=longitud_operador
 
-        #Crear los botones de la calculadora
-        boton1=self.crearBoton(7)
-        boton2=self.crearBoton(8)
-        boton3=self.crearBoton(9)
-        boton4=self.crearBoton(u"\u232B",escribir=False)
-        boton5=self.crearBoton(4)
-        boton6=self.crearBoton(5)
-        boton7=self.crearBoton(6)
-        boton8=self.crearBoton(u"\u00F7")
-        boton9=self.crearBoton(1)
-        boton10=self.crearBoton(2)
-        boton11=self.crearBoton(3)
-        boton12=self.crearBoton("*")
-        boton13=self.crearBoton(".")
-        boton14=self.crearBoton(0)
-        boton15=self.crearBoton("+")
-        boton16=self.crearBoton("-")
-        boton17=self.crearBoton("=",escribir=False,ancho=20,alto=2)
+def limpiar():
+    widget1.delete(0,END)
 
-        #Ubicar los botones con el gestor grid
-        botones=[boton1, boton2, boton3, boton4, boton5, boton6, boton7, boton8, boton9, boton10, boton11, boton12, boton13, boton14, boton15, boton16, boton17]
-        contador=0
-        for fila in range(1,5):
-            for columna in range(4):
-                botones[contador].grid(row=fila,column=columna)
-                contador+=1
-        #Ubicar el último botón al final
-        botones[16].grid(row=5,column=0,columnspan=4)
-        
-        return
+def calcular():
+    calcular_entrada = widget1.get()
+    try:
+        resultado = eval(calcular_entrada)  
+        limpiar()
+        widget1.insert(0, resultado)
+    except Exception as e:
+        limpiar()
+        widget1.insert(0, "ERROR")
 
+boton1=Button(ventana,text="1",command=lambda:iportar_numero(1)).grid(row=1,column=0,sticky=W+E)
+boton2=Button(ventana,text="2",command=lambda:iportar_numero(2)).grid(row=1,column=1,sticky=W+E)
+boton3=Button(ventana,text="3",command=lambda:iportar_numero(3)).grid(row=1,column=2,sticky=W+E)
+boton4=Button(ventana,text="4",command=lambda:iportar_numero(4)).grid(row=2,column=0,sticky=W+E)
+boton5=Button(ventana,text="5",command=lambda:iportar_numero(5)).grid(row=2,column=1,sticky=W+E)
+boton6=Button(ventana,text="6",command=lambda:iportar_numero(6)).grid(row=2,column=2,sticky=W+E)
+boton7=Button(ventana,text="7",command=lambda:iportar_numero(7)).grid(row=3,column=0,sticky=W+E)
+boton8=Button(ventana,text="8",command=lambda:iportar_numero(8)).grid(row=3,column=1,sticky=W+E)
+boton9=Button(ventana,text="9",command=lambda:iportar_numero(9)).grid(row=3,column=2,sticky=W+E)
+boton10=Button(ventana,text="0",command=lambda:iportar_numero(0)).grid(row=4,column=0,sticky=W+E)
+boton11=Button(ventana,text="=",command=lambda:calcular()).grid(row=4,column=1,columnspan=2,sticky=W+E)
+boton12=Button(ventana,text="+",command=lambda:operaciones("+")).grid(row=1,column=3,sticky=W+E)
+boton13=Button(ventana,text="-",command=lambda:operaciones("-")).grid(row=2,column=3,sticky=W+E)
+boton14=Button(ventana,text="x",command=lambda:operaciones("*")).grid(row=3,column=3,sticky=W+E)
+boton15=Button(ventana,text="/",command=lambda:operaciones("/")).grid(row=4,column=3,sticky=W+E)
+boton16=Button(ventana,text="Clear",command=lambda:limpiar()).grid(row=5,column=0,columnspan=4,sticky=W+E)
 
-    #Crea un botón mostrando el valor pasado por parámetro
-    def crearBoton(self, valor, escribir=True, ancho=9, alto=1):
-        return Button(self.ventana, text=valor, width=ancho, height=alto, font=("Helvetica",15), command=lambda:self.click(valor,escribir))
-
-
-    #Controla el evento disparado al hacer click en un botón
-    def click(self, texto, escribir):
-        #Si el parámetro 'escribir' es True, entonces el parámetro texto debe mostrarse en pantalla. Si es False, no.
-        if not escribir:
-            #Sólo calcular si hay una operación a ser evaluada y si el usuario presionó '='
-            if texto=="=" and self.operacion!="":
-                #Reemplazar el valor unicode de la división por el operador división de Python '/'
-                self.operacion=re.sub(u"\u00F7", "/", self.operacion)
-                resultado=str(eval(self.operacion))
-                self.operacion=""
-                self.limpiarPantalla()
-                self.mostrarEnPantalla(resultado)
-            #Si se presionó el botón de borrado, limpiar la pantalla
-            elif texto==u"\u232B":
-                self.operacion=""
-                self.limpiarPantalla()
-        #Mostrar texto
-        else:
-            self.operacion+=str(texto)
-            self.mostrarEnPantalla(texto)
-        return
-    
-
-    #Borra el contenido de la pantalla de la calculadora
-    def limpiarPantalla(self):
-        self.pantalla.configure(state="normal")
-        self.pantalla.delete("1.0", END)
-        self.pantalla.configure(state="disabled")
-        return
-    
-
-    #Muestra en la pantalla de la calculadora el contenido de las operaciones y los resultados
-    def mostrarEnPantalla(self, valor):
-        self.pantalla.configure(state="normal")
-        self.pantalla.insert(END, valor)
-        self.pantalla.configure(state="disabled")
-        return
-
-
-ventana_principal=Tk()
-calculadora=Interfaz(ventana_principal)
-ventana_principal.mainloop()
+ventana.mainloop()
