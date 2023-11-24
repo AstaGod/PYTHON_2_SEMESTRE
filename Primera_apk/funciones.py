7b00f579b527095d007a8028f64ff50483e7a487
AstaGod <bautistapomacrhistianalexis@gmail.com>
2023-11-24 10:50:26 -0500

    se agregaro nuevas funciones al programa mi primera_apk

--- Primera_apk/funciones.py
+++ Primera_apk/funciones.py
@@ -28,23 +28,31 @@ def f_eliminar(ventana):
 
 def f_actualizar(ventana):
     item_seleccionado = ventana.tabla_datos.selection()
-    if item_seleccionado:
-        # Obtén los nuevos datos de las cajas de texto o de donde los estés ingresando
-        nuevo_nombre = ventana.nombre_texto.get()
-        nuevo_apellido = ventana.apellidos_texto.get()
-        nuevo_celular = ventana.celular_texto.get()
-
-        # Actualiza los valores en la tabla
-        ventana.tabla_datos.item(item_seleccionado, text=nuevo_nombre, values=(nuevo_apellido, nuevo_celular))
-        f_limpiar(ventana)
+    if ventana.nombre_texto.get()=="":
+        showerror(title="SIN DATOS",message="Eres huevon no hay nada para actualizar")
     else:
-        print("Selecciona una fila para actualizar.")
+        nombre=ventana.nombre_texto.get()
+        apellidos=ventana.apellidos_texto.get()
+        celular=ventana.celular_texto.get()
+        elem_actualizar=ventana.tabla_datos.selection()
+        mensaje=askyesno(title="Actualizar",message="Estas seguro que deseas actualizar")
+        if mensaje == True:
+            f_limpiar(ventana)
+            return ventana.tabla_datos.item(elem_actualizar,text=nombre,values=(apellidos,celular))
+        else:
+            showinfo(title="NO ACTUALIZO",message="No se actualizo ningun registro")
+            f_limpiar(ventana)
 
 def f_dobleClick(ventana,event):
     elem_actualizar=ventana.tabla_datos.selection()
     captura_datos=ventana.tabla_datos.item(elem_actualizar)
-    mensaje=askokcancel(title="ACTUALIZAR",message="Desea actualizar los datos")
+    mensaje=askyesno(title="ACTUALIZAR",message="Desea actualizar los datos")
     if mensaje == True:
-        print(captura_datos["text"],captura_datos["Values"][0],captura_datos["Values"][1])
+        nombre=captura_datos["text"]
+        apellidos=captura_datos["values"][0]
+        celular=captura_datos["values"][1]
+        ventana.nombre_texto.insert(0,nombre)
+        ventana.apellidos_texto.insert(0,apellidos)
+        ventana.celular_texto.insert(0,celular)
     else:
         showinfo(title="ACTUALIZAR",message="Ningun registro seleccionado para actualizar")
