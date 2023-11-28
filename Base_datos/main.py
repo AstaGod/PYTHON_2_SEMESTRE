0d0afddde3611f79733df95d29234fdce5fc87ff
AstaGod <bautistapomacrhistianalexis@gmail.com>
2023-11-28 08:33:27 -0500

    se agregaron dos nuevas funciones eliminar y actualizar

--- Base_datos/main.py
+++ Base_datos/main.py
@@ -50,6 +50,31 @@ def insertaraAlumnos(lista):
     conn.commit()
     conn.close()
 
+def actualizarAlumno(id_alumno, nuevo_nombre, nueva_edad):
+    conn = connect("./Base_datos/tecnologico.db")
+    cursor = conn.cursor()
+
+    # Assuming you have columns 'nombre' and 'edad' in your 'Alumnos' table
+    sentencia = f"UPDATE Alumnos SET nombre = ?, edad = ? WHERE id = ?"
+    data = (nuevo_nombre, nueva_edad, id_alumno)
+
+    cursor.execute(sentencia, data)
+    conn.commit()
+    conn.close()
+
+def eliminarAlumnoPorID(id_alumno):
+    conn = connect("./Base_datos/tecnologico.db")
+    cursor = conn.cursor()
+
+    # Assuming you have a table named 'Alumnos' with a column 'id'
+    sentencia = "DELETE FROM Alumnos WHERE id = ?"
+    data = (id_alumno,)
+
+    cursor.execute(sentencia, data)
+    conn.commit()
+    conn.close()
+
+
 
 if __name__=="__main__":
     # crearConexion()
@@ -57,10 +82,18 @@ if __name__=="__main__":
     # crearTablaCurso()
     # insertaraAlumno("jory",20)
     # insertaraAlumno("chinita",12)
-    alum=[
-        ("jorycha",50),
-        ("chinita",60),
-        ("nidincita",18),
-        ("viuda negra",300)
-    ]
-    insertaraAlumnos(alum)
+    # alum=[
+    #     ("jorycha",50),
+    #     ("chinita",60),
+    #     ("nidincita",18),
+    #     ("viuda negra",300)
+    # ]
+    # insertaraAlumnos(alum)
+    # id_alumno_a_actualizar = 1  # Replace with the actual ID of the student you want to update
+    # nuevo_nombre = "Nuevo Nombre"
+    # nueva_edad = 25
+
+    # actualizarAlumno(id_alumno_a_actualizar, nuevo_nombre, nueva_edad)
+    id_alumno_a_eliminar = 25 # Replace with the actual ID of the student you want to delete
+
+    eliminarAlumnoPorID(id_alumno_a_eliminar)
